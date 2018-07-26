89b2b8ba99d3bfc640f83c4c58036b9ba6162a6e
Cgk <chengk@tuya.com>
2018-07-26 10:23:51 +0800

    add new file

---
+++ GokoTableView/UITableView+Goko/NSObject+GokoEasyCellModel.h
@@ -0,0 +1,20 @@
+//
+//  NSObject+GokoEasyCellModel.h
+//  GokoTableView
+//
+//  Created by Goko on 2018/7/26.
+//  Copyright © 2018 Goko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "GokoDefines.h"
+
+@interface NSObject (GokoEasyCellModel)
+
+@property (nonatomic, assign)               CGFloat cellRowHeight;
+@property (nonatomic, unsafe_unretained)    Class bindingCellClass;
+@property (nonatomic, copy)                 NSString * cellReuseId;
+@property (nonatomic, copy)                 GokoEasyBlock1(void, didselectBlock, UITableViewCell *, cell);
+
+@end
